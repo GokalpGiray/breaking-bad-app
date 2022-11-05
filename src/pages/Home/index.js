d03cd8fee41653841ad4b0810b5c74dafe1ad0d2
Gokalp Giray <gokalpgiray24@gmail.com>
2022-11-05 13:30:13 +0300

    The homepage has been created. Masonry was imported.

---
+++ src/pages/Home/index.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchCharacters } from '../../redux/charactersSlice';
+
+import Masonry from "react-masonry-css";
+import "./styles.css" 
+
+function Home() {
+    const characters = useSelector((state) => state.characters.items);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchCharacters());
+    }, [dispatch]);
+
+    return (
+        <div>
+            <h1>Characters</h1>
+
+            <Masonry
+                breakpointCols={3}
+                className="my-masonary-grid"
+                columnClassName="my-masonary-grid_column"
+            >
+                {
+                    characters.map(character => (
+                        <div key={character.char_id}>
+                            <img src={character.img} alt={character.name} />
+                        </div>
+                    ))
+                }
+            </Masonry>
+        </div>
+    )
+}
+
+export default Home
