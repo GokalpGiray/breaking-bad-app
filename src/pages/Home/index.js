18bbb7737349523bd7cf8f76dfd2170c02420e4b
Gokalp Giray <gokalpgiray24@gmail.com>
2022-11-07 16:32:52 +0300

    Load More button added.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,24 +3,23 @@ import { useDispatch, useSelector } from 'react-redux'
 import { fetchCharacters } from '../../redux/charactersSlice';
 
 import Masonry from "react-masonry-css";
-import "./styles.css" 
+import "./styles.css"
 import Loading from '../../components/Loading';
 import Error from '../../components/Error';
 
 function Home() {
     const characters = useSelector((state) => state.characters.items);
+    const nextPage = useSelector((state) => state.characters.page);
     const isLoading = useSelector((state) => state.characters.isLoading);
     const error = useSelector((state) => state.characters.error);
+    const hasNextPage = useSelector((state) => state.characters.hasNextPage);
+
 
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(fetchCharacters());
     }, [dispatch]);
-
-    if (isLoading) {
-        return <Loading />
-    }
 
     if (error) {
         return <Error message={error} />
@@ -44,6 +43,14 @@ function Home() {
                     ))
                 }
             </Masonry>
+
+            <div style={{ padding: "20px 0 40px 0", textAlign: "center" }}>
+                {isLoading && <Loading />}
+
+                {hasNextPage && !isLoading && <button onClick={() => dispatch(fetchCharacters(nextPage))}>Load More ({nextPage})</button>}
+
+                {!hasNextPage && <div>There is nothing to be shown</div> }
+            </div>
         </div>
     )
 }
--- src/redux/charactersSlice.js
+++ src/redux/charactersSlice.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 const charLimit = 12;
 
-export const fetchCharacters = createAsyncThunk("characters/getCharacters", async () => {
-    const res = await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}characters?limit=${charLimit}`)
+export const fetchCharacters = createAsyncThunk("characters/getCharacters", async (page) => {
+    const res = await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}characters?limit=${charLimit}&offset=${page * charLimit}`,)
     return res.data;
 });
 
@@ -13,6 +13,8 @@ export const charactersSlice = createSlice({
     initialState: {
         items: [],
         isLoading: false,
+        page: 0,
+        hasNextPage: true,
     },
     reducers: {},
     extraReducers: {
@@ -21,7 +23,11 @@ export const charactersSlice = createSlice({
         },
         [fetchCharacters.fulfilled]: (state, action) => {
             state.isLoading = false;
-            state.items = action.payload;
+            state.items = [...state.items, ...action.payload];
+            state.page += 1;
+            if (action.payload.length < 12) {
+                state.hasNextPage = false;
+            };
         },
         [fetchCharacters.rejected]: (state, action) => {
             state.isLoading = false;
